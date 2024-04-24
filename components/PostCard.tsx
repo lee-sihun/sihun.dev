b2553542d4a77a9954f4f70d8d054309a400617d
lee-sihun <lee.sihun040223@gmail.com>
2024-04-24 11:34:11 +0900

    다크모드 적용

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -14,13 +14,17 @@ export default function PostCard(post: Post): React.ReactElement {
           className="rounded-[15px] max-h-[270px]"
         />
         <div className="font-bold text-xl mt-3">{post.title}</div>
-        <div className="font-normal text-base mt-2.5">{post.description}</div>
+        <div className="font-normal text-base mt-2.5 text-[#525252] dark:text-[#A3A3A3]">
+          {post.description}
+        </div>
         {post.tags.map((tag, index) => (
           <div
             key={index}
-            className="h-8 mt-2.5 mr-2 bg-[#EDEDED] rounded-[10px] w-auto inline-flex flex-wrap justify-center items-center"
+            className="h-8 mt-2.5 mr-2 bg-[#EDEDED] dark:bg-[#262626] rounded-[10px] w-auto inline-flex flex-wrap justify-center items-center"
           >
-            <div className="font-normal text-base mx-2">{tag}</div>
+            <div className="font-normal text-base mx-2 text-[#404040] dark:text-[#B5B5B5]">
+              {tag}
+            </div>
           </div>
         ))}
       </Link>
