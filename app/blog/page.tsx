968dec3cfd6b4a1d5117426ed9d3ffc243560b1e
lee-sihun <lee.sihun040223@gmail.com>
2024-04-30 14:55:19 +0900

    카테고리 분류 기능 추가

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,18 +1,35 @@
+"use client";
 import { allPosts } from "@/.contentlayer/generated";
 import PostCard from "@/components/PostCard";
+import Category from "@/components/Category";
 import { compareDesc } from "date-fns";
+import { useState } from "react";
 
 export default function Blog() {
+  const [selectedCategory, setSelectedCategory] = useState<string>("All");
   const posts = allPosts.sort((a, b) =>
     compareDesc(new Date(a.createdAt), new Date(b.createdAt))
   );
 
+  const filteredPosts = posts.filter((post) =>
+    selectedCategory === "All" ? true : post.category === selectedCategory
+  );
+
   return (
     <div className="flex justify-center">
-      <div className="px-6 max-w-[1068px] grid grid-cols-1 gap-[60px] gap-y-20 md:grid-cols-2">
-        {posts.map((post) => (
-          <PostCard key={post._id} {...post} />
-        ))}
+      <div className="px-6 max-w-[1068px]">
+        <div className="text-[32px] font-bold mt-8 mb-3">
+          {selectedCategory} ({filteredPosts.length})
+        </div>
+        <Category
+          selectedCategory={selectedCategory}
+          onCategorySelect={setSelectedCategory}
+        />
+        <div className="grid grid-cols-1 gap-[60px] gap-y-20 md:grid-cols-2">
+          {filteredPosts.map((post) => (
+            <PostCard key={post._id} {...post} />
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ components/Category.tsx
@@ -0,0 +1,38 @@
+interface CategoryProps {
+  selectedCategory: string;
+  onCategorySelect: (category: string) => void;
+}
+
+const category: { title: string }[] = [
+  { title: "All" },
+  { title: "Develop" },
+  { title: "Design" },
+];
+
+export default function Category({
+  selectedCategory,
+  onCategorySelect,
+}: CategoryProps) {
+  return (
+    <div className="flex mb-10">
+      {category.map(({ title }) => {
+        const isSelected = selectedCategory === title;
+        const bgColor = isSelected
+          ? "bg-black dark:bg-white"
+          : "bg-white dark:bg-[#2C2C2E]";
+        const textColor = isSelected
+          ? "text-white dark:text-[#2C2C2E]"
+          : "text-black dark:text-white";
+        return (
+          <div
+            key={title}
+            className={`cursor-pointer rounded-xl w-fit h-[42px] mr-[9px] flex items-center shadow-[0_0px_10px_0px_rgba(0,0,0,0.1)] ${bgColor}`}
+            onClick={() => onCategorySelect(title)}
+          >
+            <a className={`text-[19px] mx-6 ${textColor}`}>{title}</a>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
