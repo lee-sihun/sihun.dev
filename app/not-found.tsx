ec364381de0aaa26a23551e7538c1bb450c0ee29
lee-sihun <lee.sihun040223@gmail.com>
2024-05-13 17:11:35 +0900

    feat: page not found 추가

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center font-bold">
+        <p className="text-3xl">페이지를 찾을 수 없습니다</p>
+        <Link href="/" className="text-xl underline text-blue-500">홈으로 돌아가기</Link>
+      </div>
+    </div>
+  );
+}
