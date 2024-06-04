7736e6c75609dcc91f6ecccc0087a9050b7a1b36
lee-sihun <lee.sihun040223@gmail.com>
2024-06-04 19:08:25 +0900

    feat: Image 태그 파일 설정

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: "https",
