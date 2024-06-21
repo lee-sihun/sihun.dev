413c6c9f95b237120679817400396e21b8c77ac7
lee-sihun <lee.sihun040223@gmail.com>
2024-06-21 21:16:47 +0900

    fix: 반응형 버그 수정

--- components/BlogContent.tsx
+++ components/BlogContent.tsx
@@ -17,27 +17,29 @@ export default function BlogContent() {
 
   return (
     <section className="flex justify-center flex-wrap mt-3">
-      <div className="max-w-[1068px] w-screen px-6">
-        <h2 className="text-2xl md:text-[30px] font-bold mt-2 mb-2">
-          {selectedCategory === "All" ? "All Posts" : selectedCategory} (
-          {filteredPosts.length})
-        </h2>
-        <Category
-          selectedCategory={selectedCategory}
-          onCategorySelect={setSelectedCategory}
-        />
-      </div>
-      <section className="px-6 max-w-[1068px] w-screen">
-        <div className="grid grid-cols-1 gap-[40px] gap-y-14 md:grid-cols-2">
-          {filteredPosts.map((post) => (
-            <PostCard
-              key={post._id}
-              post={post}
-              onCategorySelect={setSelectedCategory}
-            />
-          ))}
+      <div className="max-w-[1068px]">
+        <div className="max-w-[1068px] w-screen px-6">
+          <h2 className="text-2xl md:text-[30px] font-bold mt-2 mb-2">
+            {selectedCategory === "All" ? "All Posts" : selectedCategory} (
+            {filteredPosts.length})
+          </h2>
+          <Category
+            selectedCategory={selectedCategory}
+            onCategorySelect={setSelectedCategory}
+          />
         </div>
-      </section>
+        <section className="px-6 max-w-[1068px] w-screen">
+          <div className="grid grid-cols-1 gap-[40px] gap-y-14 md:grid-cols-2">
+            {filteredPosts.map((post) => (
+              <PostCard
+                key={post._id}
+                post={post}
+                onCategorySelect={setSelectedCategory}
+              />
+            ))}
+          </div>
+        </section>
+      </div>
     </section>
   );
 }
